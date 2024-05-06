be58fea86d8adfd82efd9c86d58f370fba0652bf
lyhou123 <lyhou282@gmail.com>
2024-05-06 23:19:23 +0700

    finish

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,19 +2,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: '',
+      url: 'https://mini-project-3-d048b2o1w-lyhou-phivs-projects.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: '',
+      url: 'https://mini-project-3-d048b2o1w-lyhou-phivs-projects.vercel.app/abouthttps://mini-project-3-d048b2o1w-lyhou-phivs-projects.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: '',
+      url: 'https://mini-project-3-d048b2o1w-lyhou-phivs-projects.vercel.app/policy',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
